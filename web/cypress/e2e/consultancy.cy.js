8f65cf81eb836172fb39a7b80341983fa0342aa9
Celthon Borges <celthonborges@gmail.com>
2026-03-26 21:40:55 -0300

    Update Forms Completed

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -19,10 +19,11 @@ describe('Formulário de Consultoria', () => {
             .find('select')
             .select('Individual');
 
-        // Interagindo com multipla escolha
+        // Interagindo com multipla escolha ou checkboxes
         cy.contains('label', 'Pessoa Física')
             .find('input')
-            .check(); // ou da pra usar o .click()
+            .check() // ou da pra usar o .click()
+            .should('be.checked');
 
         cy.contains('label', 'Pessoa Jurídica')
             .find('input')
@@ -33,5 +34,58 @@ describe('Formulário de Consultoria', () => {
             .find('input')
             .type('12883424705')
             .should('have.value', '128.834.247-05');
+
+        // Criando loop que verifica todas as opções disponiveis e marca todas.    
+        const discoveryChannels = [
+            'Instagram',
+            'LinkedIn',
+            'Udemy',
+            'YouTube',
+            'Indicação de Amigo'
+        ]
+
+        discoveryChannels.forEach((channel) => {
+            cy.contains('label', channel)
+                .find('input')
+                .check()
+                .should('be.checked');
+        })
+
+        //Interagindo com input do tipo file hidden, para importar arquivo externo.
+        //Importante! O arquivo DEVE estar no diretório fixtures do próprio Cypress.
+        cy.get('input[type="file"]')
+            .selectFile('./cypress/fixtures/Celthon_Borges_EN.pdf', { force: true });
+
+        //interagindo com a area de texto
+        cy.get('textarea[placeholder="Descreva mais detalhes sobre sua necessidade"]')
+            .type('dasdadasdadasdasdadadadsadadadsadasda!!!!');
+
+        // Preenchimento de texto e pressionando tecla ENTER + verificação por label pai e span filho
+        const techs = [
+            'Cypress',
+            'Postman',
+            'Jenkins',
+            'JavaScript'
+        ]
+
+        techs.forEach((tech) => {
+            cy.get('input[placeholder="Digite uma tecnologia e pressione Enter"]')
+                .type(tech)
+                .type('{enter}');
+            cy.contains('label', 'Tecnologias')
+                .parent()
+                .contains('span', tech)
+                .should('be.visible');
+        })
+
+        cy.contains('label', 'termos de uso')
+            .find('input')
+            .check();
+
+        cy.contains('button', 'Enviar formulário')
+            .click();
+
+        cy.contains('Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.')
+            .should('be.visible');
     })
 })
